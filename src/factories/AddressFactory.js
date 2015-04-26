ec98f1e52d0a08192a491596dd4fd1ed6ddb2661
Lucy Gilbert-Boyd <lucygilbert01@gmail.com>
2015-04-26 13:34:50 +0100

    Add AddressFactory.getAddressesByVendorId.

--- src/factories/AddressFactory.js
+++ src/factories/AddressFactory.js
@@ -2,6 +2,8 @@ angular.module('cpLib').factory('AddressFactory', function(ApiService) {
     return {
         getAddresses: () => ApiService.get(`/addresses`),
 
+        getAddressesByVendorId: (id) => ApiService.get(`/addresses/vendor/${id}`),
+
         createAddress: (address) => ApiService.post(`/addresses`, {address: address}),
 
         updateAddress: (id, updatedAddress) => ApiService.put(`/addresses/${id}`, updatedAddress),
