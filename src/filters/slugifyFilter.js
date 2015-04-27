214c38abcaa13b2709ac73e34d2b0fa248889c25
Amy Boyd <amy@amyboyd.co.uk>
2015-04-27 15:38:17 +0100

    Fix bugs in slugify filter.

--- src/filters/slugifyFilter.js
+++ src/filters/slugifyFilter.js
@@ -1,3 +1,8 @@
 angular.module('cpLib').filter('slugify', function() {
-    return text => String(text).replace(/ +/g, '-').toLowerCase();
+    return text => String(text)
+        .replace(/ +/g, '-')
+        .replace(/\//g, '-')
+        .replace(/-{2,}/g, '-')
+        .replace(/(.)[\.-]*$/, '$1')
+        .toLowerCase();
 });
