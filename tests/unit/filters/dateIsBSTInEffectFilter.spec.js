f99288512088fa5ab541bedca38b2c7e0374af5c
Thomas Ingram <thomasaingram@gmail.com>
2015-04-27 17:34:49 +0100

    Check if BST is in effect. Return true or false.

---
+++ tests/unit/filters/dateIsBSTInEffectFilter.spec.js
@@ -0,0 +1,19 @@
+describe('dateIsBSTInEffect filter', function() {
+    'use strict';
+
+    beforeEach(function () {
+        module('cpLib');
+    });
+
+    it('should return false for dates outside of British Summer Time', inject(function(dateIsBSTInEffectFilter) {
+        expect(dateIsBSTInEffectFilter('2015-01-01T06:00:00Z')).toBe(false);
+        expect(dateIsBSTInEffectFilter('2015-03-28T13:30:00Z')).toBe(false);
+        expect(dateIsBSTInEffectFilter('2015-10-25T18:00:00Z')).toBe(false);
+    }));
+
+    it('should return true for dates inside of British Summer Time', inject(function(dateIsBSTInEffectFilter) {
+        expect(dateIsBSTInEffectFilter('2015-03-29T06:00:00Z')).toBe(true);
+        expect(dateIsBSTInEffectFilter('2015-07-01T13:30:00Z')).toBe(true);
+        expect(dateIsBSTInEffectFilter('2015-10-24T18:00:00Z')).toBe(true);
+    }));
+});
