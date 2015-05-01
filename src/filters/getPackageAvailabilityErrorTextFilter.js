ee929690f6f8cf21b0be88671f535dd4e7c38f1d
Amy Boyd <amy@amyboyd.co.uk>
2015-05-02 00:41:05 +0100

    Add message if vendor is on holiday in getPackageAvailabilityErrorText filter.

--- src/filters/getPackageAvailabilityErrorTextFilter.js
+++ src/filters/getPackageAvailabilityErrorTextFilter.js
@@ -10,11 +10,15 @@ angular.module('cpLib').filter('getPackageAvailabilityErrorText', function() {
             return 'This meal cannot be delivered at this time.';
         } else if (!availability.isEnoughNotice) {
             return 'The vendor needs more notice to deliver at this time.';
+        } else if (!availability.isVendorOnHoliday) {
+            return 'The vendor is on holiday on that date.';
         } else {
             throw new Error('Unknown error: isPostcodeOk: ' + availability.isPostcodeOk +
                 ', isVendorDailyCapacityExceeded: ' + availability.isVendorDailyCapacityExceeded +
                 ', isDateOk: ' + availability.isDateOk + ', isTimeOk: ' + availability.isTimeOk +
-                ', isEnoughNotice: ' + availability.isEnoughNotice);
+                ', isEnoughNotice: ' + availability.isEnoughNotice +
+                ', isVendorOnHoliday: ' + availability.isVendorOnHoliday
+            );
         }
     };
 });
