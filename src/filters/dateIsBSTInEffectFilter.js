58eb17a04f395227b99fb677e77e942f9c9fd823
Amy Boyd <amy@amyboyd.co.uk>
2015-05-07 18:26:13 +0100

    dateIsBSTInEffectFilter(): support years outside of the current one.

--- src/filters/dateIsBSTInEffectFilter.js
+++ src/filters/dateIsBSTInEffectFilter.js
@@ -11,12 +11,10 @@ angular.module('cpLib').filter('dateIsBSTInEffect', function() {
             date = new Date(Date.parse(date));
         }
 
-        const now = new Date();
-
         // Loop over the 31 days of March for the current year.
         let lastSundayOfMarch;
         for (let i = 31; i > 0; i--) {
-            let tempDate = new Date(now.getFullYear(), 2, i);
+            let tempDate = new Date(date.getFullYear(), 2, i);
 
             if (tempDate.getDay() === 0) {
                 lastSundayOfMarch = tempDate;
@@ -27,7 +25,7 @@ angular.module('cpLib').filter('dateIsBSTInEffect', function() {
         // Loop over the 31 days of October for the current year.
         let lastSundayOfOctober;
         for (let i = 31; i > 0; i--) {
-            let tempDate = new Date(now.getFullYear(), 9, i);
+            let tempDate = new Date(date.getFullYear(), 9, i);
 
             if (tempDate.getDay() === 0) {
                 lastSundayOfOctober = tempDate;
--- tests/unit/filters/dateIsBSTInEffectFilter.spec.js
+++ tests/unit/filters/dateIsBSTInEffectFilter.spec.js
@@ -8,12 +8,16 @@ describe('dateIsBSTInEffect filter', function() {
     it('should return false for dates outside of British Summer Time', inject(function(dateIsBSTInEffectFilter) {
         expect(dateIsBSTInEffectFilter('2015-01-01T06:00:00Z')).toBe(false);
         expect(dateIsBSTInEffectFilter('2015-03-28T13:30:00Z')).toBe(false);
+        expect(dateIsBSTInEffectFilter('2006-03-25T13:30:00Z')).toBe(false);
         expect(dateIsBSTInEffectFilter('2015-10-25T18:00:00Z')).toBe(false);
+        expect(dateIsBSTInEffectFilter('2006-10-29T18:00:00Z')).toBe(false);
     }));
 
     it('should return true for dates inside of British Summer Time', inject(function(dateIsBSTInEffectFilter) {
         expect(dateIsBSTInEffectFilter('2015-03-29T06:00:00Z')).toBe(true);
+        expect(dateIsBSTInEffectFilter('2006-03-26T06:00:00Z')).toBe(true);
         expect(dateIsBSTInEffectFilter('2015-07-01T13:30:00Z')).toBe(true);
         expect(dateIsBSTInEffectFilter('2015-10-24T18:00:00Z')).toBe(true);
+        expect(dateIsBSTInEffectFilter('2006-10-28T18:00:00Z')).toBe(true);
     }));
 });
